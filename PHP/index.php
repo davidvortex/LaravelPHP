1606494f6f767dbb18f4304886f79db6e97949aa
Davidvortex <david@hotmail.com>
2025-02-23 17:20:51 -0600

    CrearIndex

---
+++ PHP/index.php
@@ -0,0 +1,18 @@
+<?php
+    $Nombre = "DAVID JESUS CORONADO REYES <br>";
+    $nombre = "david jesus coronado reyes <br>";
+
+    $numero = 12;
+    $numeroFloat = 123.123;
+
+    echo "el nuermo entero:". $numero ."<br> este tipo de numero es flotante:". $numeroFloat."<br>";
+
+    $cadena = "hola mundo <br>";
+
+    echo " la cadena es: $cadena";
+    echo $nombre;
+    echo $Nombre;
+
+
+
+?>
