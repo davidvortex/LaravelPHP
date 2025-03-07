8b91e57034a1ac1e61cc5719ee1b4b41cd13c5a8
davidvortex <david_2428107@hotmail.com>
2025-03-07 17:29:25 -0600

    Actualizar Introduccion

--- PHP/Introduccion.php
+++ PHP/Introduccion.php
@@ -21,4 +21,29 @@
 
     echo Animales[2];
 
+    $mi_cadena = 'david jesus';
+    echo strlen($mi_cadena);
+    echo "<br>";
+    echo mb_strlen($mi_cadena);
+    echo "<br>";
+    echo strpos($mi_cadena, "e");
+    echo "<br>";
+    echo str_contains($mi_cadena, 'jesus') ? 'Si si encuentra: ':'no se encuenta';
+    echo "<br>";
+    echo str_starts_with($mi_cadena, 'jesus') ? 'Si si encuentra: ':'no se encuenta';
+    echo "<br>";
+    $cadenas1 = 'Prueba';
+    $cadenas2 = 'prueba';
+
+    if(strcmp($cadenas1, $cadenas2) == 0){
+        echo "las dos canedas son iguales";
+    }else{
+        echo "las dos canedas son difernetes";
+    }
+
+    echo substr($cadenas1, 1);
+    echo substr($cadenas1, 1,3);
+    echo substr($cadenas1, -2);
+    $cadensa3 = "hoola mundo";
+    echo str_replace("mundo","eee", $cadenas1 );
 
