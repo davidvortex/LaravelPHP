175081fc8587f73dd31e851f272057ad37ad9dac
davidvortex <david_2428107@hotmail.com>
2025-03-11 14:52:18 -0600

    Actualizar Paginaweb

--- PHP/Introduccion.php
+++ PHP/Introduccion.php
@@ -130,20 +130,33 @@
     $nombre3 = "David Lopez";
     $nombre4 = "David Coronado";
 
-    
     $nombre2 = $_GET['nombre2'];
     $edad1 = $_GET['edad1'];
+    $roles = $_GET['roles'];
+    $sexo = $_GET['sexo'];
     echo "<p> EL nombre del usuario es: $nombre2 </p>";
     echo "<p> La edad del usuario es: $edad1 </p>";
+    echo "<p> El sexo del usuario es: $sexo </p>";
+    echo "<p> Los roles del usuario son: </p>";
+    echo "<ul>";
+        foreach($roles as $rol){
+            echo "<li>$rol</li>";
+        }
+    echo "</ul>";
     echo "<br>";
+    
+
     $nombre3 = $_POST['nombre3'];
     echo "<p> EL nombre del usuario es: $nombre3 </p>";
     echo "<p> La edad del usuario es: $edad2 </p>";
     echo "<br>";
+
     $nombre4 = $_REQUEST['nombre4'];
     echo "<p> EL nombre del usuario es: $nombre4 </p>";
     echo "<p> La edad del usuario es: $edad3 </p>";
     echo "<br>";
+
+
 
 
 
--- PHP/PaginaWeb.php
+++ PHP/PaginaWeb.php
@@ -7,22 +7,58 @@
 </head>
 <body>
     
+    Usando el motodo GET
     <form action="Introduccion.php" method="GET">
         <label>
             Nombre:
             <input type="text" name="nombre2">
         </label>
         <br>
+
         <label>
             Edad:
             <input type="number" name="edad1">
         </label>
         <br>
+
+
+        <p>Sexo:</p>
+        <label>
+            <input type="radio" name="sexo" value="Masculino">
+            Masculino:
+        </label>
+        <label>
+            <input type="radio" name="sexo" value="Femenino">
+            Femenino:
+        </label>
+        <br>
+
+
+        <p>Roles:</p>
+        <label>
+            <input type="checkbox" name="roles[]" value="Administrador">
+            Administrador:
+        </label>
+        <label>
+            <input type="checkbox" name="roles[]" value="Auxiliar">
+            Auxiliar:
+        </label>
+        <label>
+            <input type="checkbox" name="roles[]" value="Clientes">
+            Clientes:
+        </label>
+        <br>
+        <br>
         <button type="submit">Enviar</button>
     </form>
 
     <br>
-    
+    <br>
+
+
+    <hr>
+    Usando el motodo POST
+
     <form action="Introduccion.php" method="POST">
         <label>
             Nombre:
@@ -33,6 +69,11 @@
     </form>
 
     <br>
+    <br>
+
+
+    <hr>
+    Usando el motodo REQUEST
 
     <form action="Introduccion.php" method="REQUEST">
         <label>
