95efbfd33acf5211de54663e2b88816d188cc60e
davidvortex <david_2428107@hotmail.com>
2025-03-13 16:37:47 -0600

    Actualizacion de interfaces

--- PHP/interfaces/index.php
+++ PHP/interfaces/index.php
@@ -1,2 +1,34 @@
 <?php
     
+interface mi_plantilla {
+    public function setVariable($nombre);
+    public function getHtml();
+}
+
+/*
+class miplantilla implements mi_plantilla
+{
+    public function setVariable($nombre){
+
+    }
+    public function getHtml(){
+
+    }
+}
+*/
+
+interface mi_plantilla2 
+{
+    public function prueba2();
+}
+
+class mi_plantilla3 extends mi_plantilla, mi_plantilla2 {
+    public function prueba3();
+    
+}
+
+class mi_plantilla4 implements mi_plantilla3 {
+    public function prueba1(){
+        echo "prueba1";
+    }
+}
