ac82863ef4e64375f43d6c43ffa593b25cdc6ae0
davidvortex <david_2428107@hotmail.com>
2025-03-15 21:08:07 -0600

    Actualizar: FluentInterface

--- PHP/FluentInterface/index.php
+++ PHP/FluentInterface/index.php
@@ -1,16 +1,29 @@
 <?php
 
 class Personas {
-    public $nombre, $apellido1, $apellido2, $dni;
+    // esto era una manera de hacerlo despues de la version 5 de PHP
+    // ya esto no es necesario, se puede hacer directamente en el constructor
+    // public $nombre, $apellido1, $apellido2, $dni;
+
+    public function __construct(public $nombre, public $apellidoPat, public $apellidoMat, public $dni) {
+        /*
+        $this->nombre = $nombre;
+        $this->apellido1 = $apellido1;
+        $this->apellido2 = $apellido2;
+        $this->dni = $dni;
+        */
+
+    }
+
 
     public function setNombre($nombre) {
         $this->nombre = $nombre;
         return $this;
     }
 
-    public function setApellido($apellido1, $apellido2) {
-        $this->apellido1 = $apellido1;
-        $this->apellido2 = $apellido2;
+    public function setApellido($apellidoPat, $apellidoMat) {
+        $this->apellidoPat = $apellidoPat;
+        $this->apellidoMat = $apellidoMat;
         return $this;
     }
 
@@ -21,13 +34,19 @@ class Personas {
 
     public function ImprimirDatos() {
         echo "Nombre: $this->nombre <br>";
-        echo "Apellidos: $this->apellido1 $this->apellido2 <br>";
+        echo "Apellidos paterno y materno: $this->apellidoMat $this->apellidoPat <br>";
         echo "DNI: $this->dni <br>";
     }
 }
 
-$persona = new Personas();
+$persona = new Personas(nombre:"Pedro", apellidoPat: "Garcia", apellidoMat:"Lopez", dni:"87654321");
+$persona->ImprimirDatos();
+
+/*
 $persona->setNombre("Juan")
         ->setApellido("Perez", "Jimenez")
         ->setDni("12345678")
         ->ImprimirDatos();
+*/
+
+
