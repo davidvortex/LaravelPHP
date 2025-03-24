71ed2cebd4f43f9c0ed08376d4b2abe0a9346580
davidvortex <david_2428107@hotmail.com>
2025-03-23 23:56:21 -0600

    Agregar archivo Informacion.php

---
+++ PHP_y_Laravel/Informacion.php
@@ -0,0 +1,9 @@
+<?php
+
+echo "Echo por: David Jesus Coronado Reyes";
+echo "<br>";
+echo "Hago este ejercicio para practicar PHP y Laravel";
+echo "<br>";
+echo "Tengo 24 años";
+echo "<br>";
+echo "hago este repositiorio con la finalidad de aprender y mejorar mis habilidades en programacion";
