5caf57e4d0203ab1ddcfeaad0e6641a4398c62ac
davidvortex <david_2428107@hotmail.com>
2025-03-23 23:53:10 -0600

    Agregar archivo Informacion.php

---
+++ PHP_y_Laravel/Curso_Laravel/Informacion.php
@@ -0,0 +1,9 @@
+<?php
+
+echo "este archivo sirve para mostrar información de la página";
+
+echo "<br>";
+
+echo "En este repositorio se mostrara el uso de PHP y Laravel";
+echo "con la finalidad de mostrar el uso de estos lenguajes de programación";
+git add ../Informacion.php
